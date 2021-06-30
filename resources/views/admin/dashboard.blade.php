b5d1a3579bec6068ff05858f0e14ab25be44149e
rejakartans <rezaaditya.131202@gmail.com>
2021-06-30 23:32:50 +0700

    default auth

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>hello ini admin</h1>
+
+@endsection
---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>hello ini user</h1>
+
+@endsection
